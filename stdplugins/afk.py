d559463f822047eb96d2fb80a97cbe3f8ec4db97
glivinm <49768197+glivinm@users.noreply.github.com>
2020-04-22 13:23:58 +0300

    Update afk.py

--- stdplugins/afk.py
+++ stdplugins/afk.py
@@ -25,29 +25,27 @@ DEFAULTUSER = Config.ALIVE_NAME if Config.ALIVE_NAME else uname().node
 
 # ========================= CONSTANTS ============================
 AFKSTR = [
-    "`I'm busy right now. Please talk in a bag and when I come back you can just give me the bag!`",
-    "I'm away right now. If you need anything, leave a message after the beep:\n`beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep`!",
-    "`You missed me, next time aim better.`",
-    "`I'll be back in a few minutes and if I'm not...,\nwait longer.`",
-    "`I'm not here right now, so I'm probably somewhere else.`",
-    "`Roses are red,\nViolets are blue,\nLeave me a message,\nAnd I'll get back to you.`",
-    "`Sometimes the best things in life are worth waiting for…\nI'll be right back.`",
-    "`I'll be right back,\nbut if I'm not right back,\nI'll be back later.`",
-    "`If you haven't figured it out already,\nI'm not here.`",
-    "`Hello, welcome to my away message, how may I ignore you today?`",
-    "`I'm away over 7 seas and 7 countries,\n7 waters and 7 continents,\n7 mountains and 7 hills,\n7 plains and 7 mounds,\n7 pools and 7 lakes,\n7 springs and 7 meadows,\n7 cities and 7 neighborhoods,\n7 blocks and 7 houses...\n\nWhere not even your messages can reach me!`",
-    "`I'm away from the keyboard at the moment, but if you'll scream loud enough at your screen, I might just hear you.`",
-    "`I went that way\n---->`",
-    "`I went this way\n<----`",
-    "`Please leave a message and make me feel even more important than I already am.`",
-    "`I am not here so stop writing to me,\nor else you will find yourself with a screen full of your own messages.`",
-    "`If I were here,\nI'd tell you where I am.\n\nBut I'm not,\nso ask me when I return...`",
-    "`I am away!\nI don't know when I'll be back!\nHopefully a few minutes from now!`",
-    "`I'm not available right now so please leave your name, number, and address and I will stalk you later.`",
-    "`Sorry, I'm not here right now.\nFeel free to talk to my userbot as long as you like.\nI'll get back to you later.`",
-    "`I bet you were expecting an away message!`",
-    "`Life is so short, there are so many things to do...\nI'm away doing one of them..`",
-    "`I am not here right now...\nbut if I was...\n\nwouldn't that be awesome?`",
+    "`AFK! Я зайнятий зараз. Якщо щось важливе - можете це написати. Або постарайтесь не забути це`",
+    "AFK! Я тимчасово відсутній. Якщо вам щось потрібно - залиште повідомлення після сигналу:\n`ааааааааннннннііііімеееееееее`!",
+    "`AFK! Я повернусь через кілька хвилин, а якщо ні...,\nтоді прочитайте це ще раз.`",
+    "`AFK! Зараз я не тут, тому я, мабуть, десь ще.`",
+    "`AFK! Киця робить 'Мяу',\nСобачка робить кусь,\nЗалиште мені повідомлення,\nЯ скоро повернусь.`",
+    "`AFK! Іноді найкращих речей у житті варто чекати…\nЯ скоро повернусь.`",
+    "`AFK! Я скоро повернусь,\nякщо не повернусь,\nЗачекайте трошки довше.`",
+    "`AFK! Якщо ви цього ще не зрозуміли,\nЯ зараз не тут.`",
+    "`AFK! Доброго здоров'ячка.\nВас вітає FlexBot.\nКористувач зараз не в мережі.\nОчікуйте відповіді`",
+    "`AFK! Поїхав у навколосвітню подорож. Повернусь через 80 днів. Але ви можете написати СМС і я постараюсь вам чим швидше відписати`",
+    "`AFK! На даний момент я не біля клавіатури, але якщо ви будете кричати досить голосно в екран, я зможу просто почути вас.`",
+    "`AFK! Я пішов туди\n---->`",
+    "`AFK! Я пішов сюди\n<----`",
+    "`AFK! Пождіть, я тимчасово оффлайн.`",
+    "`AFK! Якби я був би тут,\nЯ би про це сказав.\n\nАле я не тут...\nТому не скажу... Скоро повернусь`",
+    "`AFK! Я зараз відсутній\nНе знаю, коли повернусь\nНадіюсь, що скоро`",
+    "`AFK! Тимчасово відстуній!\nВи можете залишити заявку. Для цього потрібні ваші паспортні дані, адреса, а також фото вашої кредитної карти з обох сторін 😂😂😂`",
+    "`AFK! Вибачте, я зараз відстуній.\nМожете поспілкуватись з FlexBot-ом прямо зараз.\nАле я не раджу цього робити...\nКраще зачекайте, коли я повернусь.`",
+    "`AFK! Життя коротке... Ще треба зробити стільки речей...\nІ я роблю одну з них...`",
+    "`AFK! Мене тут зараз немає...\nале якби був...\n\nЧи не було б це судово?`",
+    
 ]
 # ============================================
 
@@ -90,9 +88,9 @@ async def _(event):
             afkb_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFKB = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
-            await borg.send_message(event.chat_id, f"**My King 👑 {DEFAULTUSER} 👑 is Going afk!** __because ~ {reason}__")
+            await borg.send_message(event.chat_id, f"** Користувач {DEFAULTUSER} перейшов у AFK,** __тому що він ~ {reason}__")
         else:
-            await borg.send_message(event.chat_id, f"**My King 👑 {DEFAULTUSER} 👑 is Going afk!**")
+            await borg.send_message(event.chat_id, f"**Користувач 👑 {DEFAULTUSER} перейшов у AFK! Причина невідома...**")
         await asyncio.sleep(5)
         await event.delete()
         try:
@@ -116,7 +114,7 @@ async def set_not_afkb(event):
     total_afkb_time = str(afkb_end - afkb_start)
     current_message = event.message.message
     if ".afkb" not in current_message and "yes" in USER_AFKB:  # pylint:disable=E0602
-        shite = await borg.send_message(event.chat_id, "__My Master is Back!__\n**He is No Longer afk.**\n `Was afk for:``" + total_afkb_time + "`")
+        shite = await borg.send_message(event.chat_id, "__КОРИСТУВАЧ ПОВЕРНУВСЯ__\n**USER IS BACK.**\n `Я був AFK :``" + total_afkb_time + "`")
         try:
             await borg.send_message(  # pylint:disable=E0602
                 Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
@@ -188,10 +186,10 @@ async def on_afkb(event):
             else:
                 afkb_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"My Master {DEFAULTUSER} Is **afk since** {total_afkb_time}" + \
-            f"\n__and HE may be back soon__\n**Because my King is** {reason}" \
+        message_to_reply = f"Користувач {DEFAULTUSER} відсутній уже* {total_afkb_time}" + \
+            f"\n__але скоро повернеться__\nВін сказав, що зараз він: {reason}" \
             if reason \
-            else f"My King 👑 {DEFAULTUSER} 👑 is **afk Since** {total_afkb_time}. \nand My King has left a word for you only: \n{AFKSK}\n`.` "
+            else f"{AFKSK}\n`.Користувач {DEFAULTUSER} 👑 відсутній уже {total_afkb_time}.` "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afkb_message:  # pylint:disable=E0602
